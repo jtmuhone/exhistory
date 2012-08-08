1fad058bb596486178a8e18f9eda12aff762045a
Joonas Muhonen <jtm@iki.fi>
2012-08-08 18:50:50 +0300

    Fixes, uses now json parsing and session storage.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Session history management support for Internet Explorer
 
-A javascript library to add support for
+A javascript library to add limited support for
 [session history management](http://caniuse.com/#feat=history) in
 Internet Explorer < 10.
 
-Alpha stage.
+Uses [JSON parsing](http://caniuse.com/json) and [session storage](http://caniuse.com/namevalue-storage), limiting IE support to > 7.
+
+Early beta stage.
--- exhistory.js
+++ exhistory.js
@@ -1,10 +1,20 @@
-if (! "pushState" in window.history) {
-    window.history.states = {};
+if (typeof window.history.pushState !== "function") {
+    
+    history.enablePopstate = true;
+    if (! sessionStorage.getItem("exhistory.states")) {
+        sessionStorage.setItem("exhistory.states", "{}");
+        console.log("Init states");
+    }
     window.addEventListener("hashchange",
 			    (function () {
 				if ("onpopstate" in window &&
 				    location.hash.indexOf("#!/") == 0) {
-				    window.onpopstate({state: window.history.states[location.hash]});
+				    if (history.enablePopstate) {
+					var states = JSON.parse(sessionStorage.getItem("exhistory.states"));
+					window.onpopstate({state: states[location.hash]});
+				    } else {
+					history.enablePopstate = true;
+				    }
 				}
 			    }), false);
     window.history.pushState = function(data, title, url) {
@@ -18,10 +28,12 @@ if (! "pushState" in window.history) {
 	} else {
 	    var fullUrl = "#!/" + url;
 	}
-	window.history.states[fullUrl] = data;
+	var states = JSON.parse(sessionStorage.getItem("exhistory.states"));
+	states[fullUrl] = data;
+	sessionStorage.setItem("exhistory.states", JSON.stringify(states));
+	history.enablePopstate = false;
 	location.hash = fullUrl;
     };
 
     window.history.replaceState = window.history.pushState;
-
 }
